8b6dc5fc947fc317e8792ad43285f4053156db4d
kodashimayuhi <yslm0702@icloud.com>
2020-12-17 23:45:03 +0900

    配列

---
+++ java study/task1_2.java
@@ -0,0 +1,32 @@
+public class task1_2 {
+
+    public static void main(String[] args) {
+
+        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+        String[] country = {"america", "korea", "japan"};
+
+        // ② ①で作成した配列の要素数を出力してください。
+        System.out.println(+country.length);
+
+        /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+         *   1番目（先頭）の要素に 「りんご」 を代入
+         *   2番目の要素に 「もも」 を代入
+         *   3番目の要素に 「ぶどう」 を代入
+         */
+         String[] strArray ={"りんご","もも","ぶどう"};
+
+        // ④ ③で作成した配列の2番目の要素を出力しなさい。
+        strArray[1] ="もも";
+        System.out.println(strArray[1]);
+
+        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+         *  [配列の作成と、初期値の代入]
+         */
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // [配列の２番目と５番目の要素を出力]
+        System.out.println(intArray[1] + intArray[4]);
+
+    }
+}
