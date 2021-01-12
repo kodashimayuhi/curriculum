d2d318b6044419efbbdb477f3934fd063225645d
kodashimayuhi <yslm0702@icloud.com>
2021-01-12 23:55:17 +0900

    オーバーロード・継承・オーバーライド
    2-3

---
+++ JAVA_STUDY/2-3/src/study/Calculator.java
@@ -0,0 +1,17 @@
+package study;
+
+public class Calculator {
+
+    protected static int plusFirst(int a) {
+        return a + 1;
+    }
+
+    protected static int plusSecond(int a, int b) {
+        return a + b;
+    }
+
+    protected static int plusThird(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
