d18317d0bf4bd91031f28b84b28db419b9ed554f
kodashimayuhi <yslm0702@icloud.com>
2021-01-20 23:27:47 +0900

    （再）パッケージ
    2-4

--- JAVA_STUDY/2-4/src/study/Study.java
+++ JAVA_STUDY/2-4/src/study/Study.java
@@ -1,6 +1,6 @@
 package study;
 
-//②Fruits.javaをインポートしなさい。
+import constants.FruitsConstants;
 import fruits.Fruits;
 
 /**
@@ -12,15 +12,19 @@ import fruits.Fruits;
 */
 public class Study {
 
+
 public static void main(String[] args) {
 
       /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
        *     また、課題の画像と同じ表示になるように
        *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
        */
-	  Fruits fruits = new Fruits();
-	  fruits.printFruits("レモン", 20);
 	  
+	  Fruits.printFruits(FruitsConstants.FRUITS_LEMON_04, FruitsConstants.FRUITS_COUNT_20);
+      
+}
 
-  }
+      
+
+     
 }
