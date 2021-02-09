aae315e26eadde62f977582d0d198a4b3b39c8c8
kodashimayuhi <yslm0702@icloud.com>
2021-02-10 00:03:23 +0900

    （提出用）例外処理・スタックトレース
    4-1

--- JAVA_STUDY/4-1/src/exception/ExException.java
+++ JAVA_STUDY/4-1/src/exception/ExException.java
@@ -60,7 +60,6 @@ public class ExException {
 	 case CONST_EXCEPTION_TRIGER_ARRAY_OUT_OF_BOUNDS:
 	  // 問②: 「throw」を使用せずに「ArrayIndexOutOfBoundsException」を発生させる処理を記述しなさい。
 	  // Tips: ご自身で配列を準備してください（使用する配列の型、要素数は自由）
-	  // ここへ記述
 		 try {
 			 int[] array = {1, 2, 3};
 			 System.out.print(array[5]);
